8f9cd2cdcf24181124cfb0fc3deff76274f3d612
Otrebor0417 <roborobert185@gmail.com>
2026-05-03 22:43:02 -0700

    fefefefe

--- Unit3SoundAndEffectP7Diaz/Assets/Scripts/SpawnManager.cs
+++ Unit3SoundAndEffectP7Diaz/Assets/Scripts/SpawnManager.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
+    public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
     private float repeatRate = 2;
+    private PlayerController playerControllerScript;
+    private int randomObstacle;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -19,6 +23,11 @@ public class SpawnManager : MonoBehaviour
     }
     void SpawnObstacle()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+      if (playerControllerScript.gameOver == false)
+        {
+            randomObstacle = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[randomObstacle], spawnPos, obstaclePrefabs[randomObstacle].transform.rotation);
+        }
+       
     }
 }
